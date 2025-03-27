c8c951fd51a59e9d315fca0e3defe796ee7aa8c1
austin.allen <austin.allen@kitware.com>
2025-03-27 15:06:42 -0400

    Add files for scoring algorithms

---
+++ banff_aid/features/banff_lesion_scores/ct/cli/tubular_atrophy/tubular_atrophy.py
@@ -0,0 +1,9 @@
+def calculate_ct(tubules: dict[str, dict]) -> int:
+    """Calculate ct Banff lesion score.
+
+    Args:
+    tubules (dict[str, dict]): Dictionary containing JSON-formatted annotations
+                              for tubules.
+
+    Return value (int): ct score.
+    """
