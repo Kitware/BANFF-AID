1c692e7d5e9a3b0e225fa15ee00758fc034fd866
austin.allen <austin.allen@kitware.com>
2025-03-27 15:06:42 -0400

    Add files for scoring algorithms

---
+++ banff_aid/features/banff_lesion_scores/ci/cli/interstitial_fibrosis/interstitial_fibrosis.py
@@ -0,0 +1,9 @@
+def calculate_ci(tubules: dict[str, dict]) -> int:
+    """Calculate ci Banff lesion score.
+
+    Args:
+    tubules (dict[str, dict]): Dictionary containing JSON-formatted annotations
+                              for tubules.
+
+    Return value (int): ci score.
+    """
